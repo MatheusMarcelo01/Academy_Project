694b3398af18f3f6a18e9917b85f363faeca0788
MatheusMarcelo01 <matheusmpmenezes@protonmail.com>
2023-07-11 21:34:17 -0300

    inicio da codificação

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<h2>test</h2>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import imgHome from '../assets/musculacao.jpg'
+</script>
+
+<section class="intro" style="background-image: url({imgHome})">
+    <div class="intro__wrapper wrap">
+        <h1>Você está pronto para o desafio? 💪    </h1>
+        <p>A Academia Local está pronta para te ajudar a chegar mais longe e alcançar sua melhor performance.</p>
+        <a href="/sobre">Sobre</a>
+    </div>
+  
+</section> 
+
+<style>
+    .intro {
+        min-height: 360px;
+        background-size: cover;
+        background-position: center;
+        display:grid;
+        align-content: center;
+        justify-content: start;
+        position: relative;
+
+    }
+    .intro::after{
+        content: '';
+        position: absolute;
+        inset: 0;
+        background-color: rgba(0,0,0,0.55)
+    }
+
+    .intro__wrapper{
+        z-index: 1;
+    }
+
+</style>

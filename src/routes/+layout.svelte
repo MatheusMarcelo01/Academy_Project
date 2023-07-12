b960f44474dd45581ac21bedc3405fb7e59f2af8
MatheusMarcelo01 <matheusmpmenezes@protonmail.com>
2023-07-11 21:05:17 -0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import '../global.css'
+    import  '@fontsource/roboto-condensed';
+    import  '@fontsource/roboto-condensed/700.css';
+    import '@fontsource/cabin';
+
+
+</script>
+
+<slot/>
